18556f4dae98f97be2d4533227eb8b76d2d04011
vbuots@icloud.com <vbuoc@yandex.ru>
2022-12-13 10:46:54 +0400

    heads

--- app/head.js
+++ app/head.js
@@ -1,6 +1,6 @@
 const Head = () => (
   <>
-    <title>My syte</title>
+    <title>ГОРОДСКАЯ ПОХОРОННАЯ СЛУЖБА</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
   </>
