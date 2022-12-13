d88a1e81dc0ba1aea9e23a6dcb60940a20b48c54
ipyguru <vbuots@icloud.com>
2022-12-13 13:04:12 +0400

    Update head.js

--- app/head.js
+++ app/head.js
@@ -1,6 +1,8 @@
+import { contacts } from "../constants";
+
 const Head = () => (
   <>
-    <title>ГОРОДСКАЯ ПОХОРОННАЯ СЛУЖБА</title>
+    <title>{contacts.companyName}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
   </>
